9dcaf970f696493b330908329df6539fab6d011d
Gangaithiri241991 <ajimansamy29@gmail.com>
2023-03-05 19:53:10 +0530

    add text

---
+++ script.js
@@ -0,0 +1,112 @@
+
+   
+  
+    function displayMessage() {
+        const message = "Happy Independence Day!";
+        const textarea = document.getElementById("myParagraph");
+        textarea.textContent = message;
+
+        textarea.setAttribute("id","sa");
+       
+        
+      }
+      
+      
+     
+   const para=document.createElement("div");
+   para.id="posi";
+   para.textContent="CALL BACK FUNCTION";
+   document.body.appendChild(para);
+   
+   
+   /*function addEventListener(){
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", displayMessage);*/
+   
+  function one(cb = () => {}) {
+    console.log('10')
+    cb();
+    
+}
+
+function two(cb = () => {}) {
+    console.log('9')
+
+    cb();
+}
+
+function three(cb = () => {}) {
+    console.log('8')
+
+    cb();
+}
+
+function four(cb = () => {}) {
+    console.log('7')
+
+    cb();
+}
+
+function five(cb = ()  =>{}) {
+    console.log('6')
+    cb();
+} 
+
+function six(cb= () =>{}) {
+    console.log('5')
+    cb();
+} 
+
+function seven(cb = () =>{}) {
+    console.log('4')
+    cb();
+} 
+
+function eight(cb = () =>{}) {
+    console.log('3')
+    cb();
+} 
+
+function nine(cb = () =>{}) {
+    console.log('2')
+    cb();
+} 
+
+function ten(cb = () =>{}) {
+    console.log('1')
+    cb();
+    
+
+    console.log("Happy Independence Day");
+} 
+  
+
+
+ function greet() {
+  one(() => setTimeout(() =>{
+   two(() => setTimeout(() =>{
+    three(() => setTimeout(() =>{
+     four(() => setTimeout(() =>{
+      five(() => setTimeout(() =>{
+       six(() => setTimeout(() => {
+        seven(() => setTimeout(() =>{
+         eight(() => setTimeout(() => {
+            nine(() => setTimeout(() => {
+                
+            ten(()=>setTimeout(()=>{  
+                displayMessage();
+                
+            }),1000);       
+            }), 1000);       
+         }), 1000);
+        },1000));
+       },1000));
+      }, 1000));
+     }, 1000));
+    }, 1000));
+   }, 1000));
+  }, 1000));
+ }
+
+greet();
+
